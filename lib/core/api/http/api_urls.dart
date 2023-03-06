8c1e686fbf51633f79c16adc0dddc210b94e69a3
Mosab Omari <56319444+mosab-omari@users.noreply.github.com>
2023-03-06 16:05:58 +0300

    add change language for notifications

--- lib/core/api/http/api_urls.dart
+++ lib/core/api/http/api_urls.dart
@@ -1,18 +1,21 @@
 import '../../constants/app_settings.dart';
 
-class   ApiURLs {
+class ApiURLs {
   ///baseUrl
   static const String baseUrl = AppSettings.baseUrl;
   static const String servicesUrl = '${baseUrl}services/';
   static const String appUrl = '${servicesUrl}app/';
   static const String authUrl = '${baseUrl}TokenAuth/';
-  static const String notitfyScreenUrl = '${baseUrl}services/app/Call/NotifyScreenToJoin';
-  static const String CancleCallRequestUrl = '${baseUrl}services/app/Call/CancleRequest';
+  static const String notitfyScreenUrl =
+      '${baseUrl}services/app/Call/NotifyScreenToJoin';
+  static const String CancleCallRequestUrl =
+      '${baseUrl}services/app/Call/CancleRequest';
 
   ///USER
   static const String userUrl = '${appUrl}User/';
   static const String setFCMToken = '${userUrl}setCurrentFcmToken/';
   static const String loginUrl = '${authUrl}Authenticate';
+  static const String changeLanguageUrl = '${userUrl}changeLanguage';
 
   ///Calls
   static const String callUrl = '${appUrl}Call/';
